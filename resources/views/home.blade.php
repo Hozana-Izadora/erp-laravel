a6e501dd58d1cc4ece066fa583fcc1ae17b9d40f
hozana-izadora <izadoraferreir@gmail.com>
2021-09-02 09:33:49 -0300

    Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,23 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
-
-@section('content_header')
-    <h1>Dashboard</h1>
-@stop
-
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"> ERP Laravel</div>
+                <div class="card-body">
+                    @if(session('status'))
+                    <div class="alert alert-success">
+                        {{session('status')}}
+                    </div>
+                    @endif
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
-
+                    Você foi logado com sucesso!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
 
